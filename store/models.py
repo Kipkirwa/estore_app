f5354f469d5619bf4a6c2acb2f041a422a90fdb1
Kipkirwa <gleitich@gmail.com>
2024-02-29 20:30:08 +0300

    setting and implementing cart

--- store/models.py
+++ store/models.py
@@ -4,7 +4,38 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
 
-# Cartegories of products
+# Create Customer Profile
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	date_modified = models.DateTimeField(User, auto_now=True)
+	phone = models.CharField(max_length=20, blank=True)
+	address1 = models.CharField(max_length=200, blank=True)
+	address2 = models.CharField(max_length=200, blank=True)
+	city = models.CharField(max_length=200, blank=True)
+	state = models.CharField(max_length=200, blank=True)
+	zipcode = models.CharField(max_length=200, blank=True)
+	country = models.CharField(max_length=200, blank=True)
+	old_cart = models.CharField(max_length=200, blank=True, null=True)
+
+	def __str__(self):
+		return self.user.username
+
+# Create a user Profile by default when user signs up
+def create_profile(sender, instance, created, **kwargs):
+	if created:
+		user_profile = Profile(user=instance)
+		user_profile.save()
+
+# Automate the profile thing
+post_save.connect(create_profile, sender=User)
+
+
+
+
+
+
+
+# Categories of Products
 class Category(models.Model):
 	name = models.CharField(max_length=50)
 
@@ -27,6 +58,7 @@ class Customer(models.Model):
 
 	def __str__(self):
 		return f'{self.first_name} {self.last_name}'
+
 
 
 # All of our Products
